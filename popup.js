be52326778e095fbd17f759124577bbd6304cea0
lsw@autobotstech.com <lsw@autobotstech.com>
2020-08-10 13:34:05 +0800

    fix:test

--- popup.js
+++ popup.js
@@ -2,4 +2,26 @@ $(function () {
     $('#name').keyup(function() { 
         $('#great').text('hello ' + $('#name').val())
     })
+
+
+    chrome.storage.sync.get('total', (budget) => { 
+        if (budget.total) {
+            $('#total').text(budget.total)
+        }
+    })
+
+    $('#spendAmount').click(() => { 
+        chrome.storage.sync.get('total', (budget) => { 
+            let newTotal = 0
+            if (budget.total) {
+                newTotal += parseInt(budget.total)
+            }
+            let amount = $('#amount').val()
+            if (amount) {
+                newTotal += parseInt(amount)
+            }
+            chrome.storage.sync.set({'total': newTotal})
+            $('#total').text(newTotal)
+        })
+    })
 })
